c680e40879547b13b2a11f354b881ad28a9a5d99
Luis Filipe <luisfilipels@hotmail.com>
2021-05-31 17:57:01 -0300

    Removed debug code

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,20 +11,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        System.out.println(1);
         Stage popup = new Stage();
         FXMLLoader loader = new FXMLLoader(
                 getClass().getResource("popupName.fxml")
         );
-        System.out.println(2);
         popup.initStyle(StageStyle.UTILITY);
         Parent root2 = loader.load();
-        System.out.println(3);
         popup.setTitle("Leitura de dados");
         popup.setResizable(false);
         popup.setScene(new Scene(root2, 400, 450));
         popup.show();
-        System.out.println(4);
         popup.setAlwaysOnTop(true);
         popup.toFront();
     }
